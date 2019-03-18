30eb0762e0e64f9e67cef4ba413b32d2a3f91226
Philippe Bruhat (BooK) <philippe.bruhat@booking.com>
2019-03-18 17:14:35 +0100

    First incomplete implementation

--- lib/Dotenv.pm
+++ lib/Dotenv.pm
@@ -1,3 +1,68 @@
+package Dotenv;
+
+use Carp       ();
+use Path::Tiny ();
+
+my $parse = sub {
+    my ( $string, $env ) = @_;
+    my %kv;
+    for my $line ( split /$/, $string ) {
+        chomp($line);
+        my ( $k, $v ) = split /\s*=\s*/, $line, 2;
+
+        # TODO: munging of "" '' $vars
+
+        $kv{$k} = $v;
+    }
+    return %kv;
+};
+
+sub load {
+    my ( $class, @sources ) = @_;
+    my %env;
+    %env = %ENV if !defined wantarray;
+
+    for my $source (@sources) {
+        Carp::croak "Can't handle an unitialized value"
+          if !defined $source;
+        my %kv;
+	my $ref = ref $source;
+        if ( $ref eq '' ) {
+            %kv = $parse->( $source, \%env );
+        }
+        elsif ( $ref eq 'HASH' ) {    # bare hash ref
+            %kv = %$source;
+        }
+        elsif ( $ref eq 'SCALAR' ) {
+            %kv = $parse->( $$source, \%env );
+        }
+        elsif ( $ref eq 'GLOB' ) {
+            local $/;
+            %kv = $parse->( scalar <$source>, \%env );
+            close $content;
+        }
+        elsif ( eval { $source->can('getline') } ) {
+            local $/;
+            %kv = $parse->( scalar $source->getline, \%env );
+            $source->close;
+        }
+        else {
+            Carp::croak "Don't know how to handle '$source'";
+        }
+
+        # don't overwrite anything that already exists
+        %env = ( %kv, %env );
+    }
+
+    return !defined wantarray
+      ? %ENV = %env    # replace %ENV in void context
+      : %env;          # return the parsed pairs otherwise
+}
+
+'.env';
+
+__END__
+
 =pod
 
 =head1 NAME
